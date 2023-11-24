dac97058c0006b4f78ac1fe07585ae0b7a9bc908
jakubwozny16 <129044737+jakubwozny16@users.noreply.github.com>
2023-11-24 11:47:02 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const mysql = require('mysql2');
+const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'test'
+});
+
+function dodaj(obj,tab) {
+  const query = `INSERT INTO ${tab} SET ?`;
+  connection.query(query, obj, (error, results, fields) => {
+    if (error) throw error;
+  });
+}
+function aktaualizuj(id,obj,tab) {
+  const query = `UPDATE ${tab} SET ? WHERE id = ?`;
+  connection.query(query, [obj, id], (error, results, fields) => {
+    if (error) throw error;
+  });
+}
+function usun(id,tab) {
+  const query = `DELETE FROM ${tab} WHERE id = ?`;
+  connection.query(query, [id], (error, results, fields) => {
+    if (error) throw error;
+  });
+}
+
+
+function pobierz(parametr,tab) {
+  if(parametr){
+    parametr = 'WHERE `name` = "Page" AND `age` > 45';
+  }
+
+ let query = 'SELECT * FROM ' +tab+ 'parametr';  
+  connection.query()
+      query,
+      function(err, results, fields) {
+        console.log(results); 
+        console.log(fields);
+        return fields;
+      }
+}
+
+
+
+app.get("/", (req, res) => {
+  res.send(data());
+});
+
+app.listen(8080);
+
+function usun(id){
+
+}
+
+function pobierz(parametr) {
+
+}
+
+
+
+app.get("/", (req, res) => {
+  res.send(data());
+});
+
+app.listen(8080);
