143dbd33396c8a70cb6faa20cc1e8e6da745cc21
jakubwozny16 <129044737+jakubwozny16@users.noreply.github.com>
2023-12-01 11:42:14 +0100

    index.js

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
 const express = require("express");
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 const app = express();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'test'
+  host: "109.196.50.236:10000",
+  user: "zsti",
+  password: "EciePecie666!",
+  database: "osoby",
 });
 
-function dodaj(obj,tab) {
+function dodaj(obj, tab) {
   const query = `INSERT INTO ${tab} SET ?`;
   connection.query(query, obj, (error, results, fields) => {
     if (error) throw error;
   });
 }
-function aktaualizuj(id,obj,tab) {
+function aktaualizuj(id, obj, tab) {
   const query = `UPDATE ${tab} SET ? WHERE id = ?`;
   connection.query(query, [obj, id], (error, results, fields) => {
     if (error) throw error;
   });
 }
-function usun(id,tab) {
+function usun(id, tab) {
   const query = `DELETE FROM ${tab} WHERE id = ?`;
   connection.query(query, [id], (error, results, fields) => {
     if (error) throw error;
   });
 }
 
-
-function pobierz(parametr,tab) {
-  if(parametr){
+function pobierz(parametr, tab) {
+  if (parametr) {
     parametr = 'WHERE `name` = "Page" AND `age` > 45';
   }
 
- let query = 'SELECT * FROM ' +tab+ 'parametr';  
-  connection.query()
-      query,
-      function(err, results, fields) {
-        console.log(results); 
-        console.log(fields);
-        return fields;
-      }
+  let query = "SELECT * FROM " + tab + "parametr";
+  connection.query();
+  query,
+    function (err, results, fields) {
+      console.log(results);
+      console.log(fields);
+      return fields;
+    };
 }
-
-
 
 app.get("/", (req, res) => {
   res.send(data());
@@ -51,15 +49,9 @@ app.get("/", (req, res) => {
 
 app.listen(8080);
 
-function usun(id){
+function usun(id) {}
 
-}
-
-function pobierz(parametr) {
-
-}
-
-
+function pobierz(parametr) {}
 
 app.get("/", (req, res) => {
   res.send(data());
